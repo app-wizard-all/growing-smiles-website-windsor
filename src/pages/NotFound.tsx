9229dac456f9629976d45054e9b2f81b9ff10f14
app-wizard-all <abhinavdahuja9@gmail.com>
2025-06-12 07:45:53 +0530

    Update NotFound.tsx

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,6 @@
 import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
+import Navbar from '../components/Navbar';
 
 const NotFound = () => {
   const location = useLocation();
